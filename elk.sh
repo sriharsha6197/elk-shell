4f58ce1fe23e8a2dafe51a72348629a8848a7166
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-06-13 22:12:23 +0530

    push elk shell script

---
+++ elk.sh
@@ -0,0 +1,34 @@
+echo copy repo
+vim /etc/yum.repos.d/elastic.repo
+[elasticsearch]
+name=Elasticsearch repository for 8.x packages
+baseurl=https://artifacts.elastic.co/packages/8.x/yum
+gpgcheck=0
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+
+echo installing nginx
+dnf install nginx -y
+systemctl enable nginx
+systemctl start nginx
+
+echo install elasticsearch
+dnf install elasticsearch -y
+systemctl enable elasticsearch
+systemctl start elasticsearch
+systemctl daemon-reload
+
+echo install kibana
+dnf install kibana -y
+systemctl enable kibana
+systemctl start kibana
+systemctl daemon-reload
+
+echo install logstash
+dnf install logstash -y
+systemctl enable logstash
+systemctl start logstash
+
+
