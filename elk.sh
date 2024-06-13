28d3ebd8ae83ed0a64cb0e5a9310767463841e81
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-06-13 22:25:53 +0530

    c

--- elk.sh
+++ elk.sh
@@ -1,13 +1,13 @@
 echo copy repo
 
-echo '[elasticsearch]
+echo [elasticsearch]
 name=Elasticsearch repository for 8.x packages
 baseurl=https://artifacts.elastic.co/packages/8.x/yum
 gpgcheck=0
 gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
 enabled=1
 autorefresh=1
-type=rpm-md' >>/etc/yum.repos.d/elastic.repo
+type=rpm-md >>/etc/yum.repos.d/elastic.repo
 
 echo installing nginx
 dnf install nginx -y
